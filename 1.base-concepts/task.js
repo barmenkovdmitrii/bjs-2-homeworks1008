a583c38a8fc8c88bd5950d41aca31e5bbe1af450
Дмитрий <bds@porter.pro>
2022-08-13 11:33:05 +0300

    1308_1132

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,19 @@
 function solveEquation(a, b, c) {
+    let arr;
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+    if ( discriminant == 0){
+        arr = [-b / 2 * a];
+    }
+
+    if (discriminant > 0){
+        arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+    }
+
+    return arr; // array
+}
+
+function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
   let d = b**2-4*a*c;
