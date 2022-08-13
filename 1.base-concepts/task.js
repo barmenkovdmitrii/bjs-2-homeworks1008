3a077fc29858d07f2f0823776354b842c1229f9b
Дмитрий <bds@porter.pro>
2022-08-13 11:38:33 +0300

    1308_1138

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -13,19 +13,6 @@ function solveEquation(a, b, c) {
     return arr; // array
 }
 
-function solveEquation(a, b, c) {
-  let arr = [];
-  // код для задачи №1 писать здесь
-  let d = b**2-4*a*c;
-  if (d < 0) {
-
-  }
- else if (d > 0) {
-
-  }
-  return arr; // array
-}
-
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
