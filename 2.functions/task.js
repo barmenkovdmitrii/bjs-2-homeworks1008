3d5119858000a3c20a2e0f99086706499efe7471
barmenkovdmitrii <bds@porter.pro>
2022-08-17 12:55:22 +0300

    PC

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,19 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
+let arr = [1,4,67,2];
 
+function getArrayParams(arr) {
+  let min = arr[0], max = arr[0], sum = 0, avg; // Почему Вадим присвоил массив к переменным
+for ( let i = 0; i < arr.length; i++) {
+  if ( arr[i] > max) {
+    max = arr[i];
+      }
+  if ( arr[i] < min) {
+    min = arr[i];
+  }
+  sum +=  arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
   // Ваш код
 
   return { min: min, max: max, avg: avg };
@@ -9,8 +21,10 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+for (let i = 0; i < arr.length[i]; i++) {
+  sum += arr[i];
+}
   // Ваш код
 
   return sum;
@@ -21,7 +35,7 @@ function makeWork(arrOfArr, func) {
 
   // Ваш кода
   // for ...
-  
+
   return max;
 }
 
